9329f2ad9a8c49085476e37701174d3e26687768
NoSequel <nosequel@protonmail.com>
2021-03-24 23:07:56 +0000

    attempt to fix again

--- v1_7_R4/src/main/java/io/github/nosequel/tab/v1_7_r4/v1_7_R4TabAdapter.java
+++ v1_7_R4/src/main/java/io/github/nosequel/tab/v1_7_r4/v1_7_R4TabAdapter.java
@@ -73,7 +73,7 @@ public class v1_7_R4TabAdapter extends TabAdapter {
         if (skinData.length >= 1 && !skinData[0].isEmpty() && !skinData[1].isEmpty()) {
             boolean shouldUpdate = false;
 
-            if(profile.getProperties().containsKey("textures") && profile.getProperties().get("textures") instanceof Property) {
+            if(profile.getProperties().containsKey("textures") && new ArrayList<>(profile.getProperties().get("textures")).get(0) != null) {
                 final Property property = (Property) profile.getProperties().get("textures");
                 shouldUpdate = !property.getSignature().equals(skinData[1]) || !property.getValue().equals(skinData[0]);
             }
